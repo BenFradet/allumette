324ed73eed96e2394080c752d8759de9dbf64393
benfradet <benjamin.fradet@gmail.com>
2024-11-09 22:11:09 +0100

    hof tests

--- src/hof.rs
+++ src/hof.rs
@@ -12,7 +12,7 @@ where
     result.into_iter()
 }
 
-fn zipWith<I1, I2, F, A, B, C>(i1: I1, i2: I2, f: F) -> impl Iterator<Item = C>
+fn zip_with<I1, I2, F, A, B, C>(i1: I1, i2: I2, f: F) -> impl Iterator<Item = C>
 where 
     I1: IntoIterator<Item = A>,
     I2: IntoIterator<Item = B>,
@@ -47,7 +47,7 @@ fn negs<I>(i: I) -> impl Iterator<Item = f64> where I: IntoIterator<Item = f64> 
 }
 
 fn adds<I>(i1: I, i2: I) -> impl Iterator<Item = f64> where I: IntoIterator<Item = f64> {
-    zipWith(i1, i2, add)
+    zip_with(i1, i2, add)
 }
 
 fn sums<I>(i: I) -> f64 where I: IntoIterator<Item = f64> {
@@ -56,4 +56,39 @@ fn sums<I>(i: I) -> f64 where I: IntoIterator<Item = f64> {
 
 fn prods<I>(i: I) -> f64 where I: IntoIterator<Item = f64> {
     reduce(i, mul, 1.)
+}
+
+
+use proptest::prelude::*;
+
+proptest! {
+    #[test]
+    fn test_map(array in prop::array::uniform10(0f64..)) {
+        let res = map(array, |a| a);
+        assert!(array.into_iter().zip(res).all(|(a, b)| a == b));
+    }
+
+    #[test]
+    fn test_negs(array in prop::array::uniform10(0f64..)) {
+        let res = negs(array);
+        assert!(res.into_iter().all(|a| a < 0.));
+    }
+
+    #[test]
+    fn test_adds(array in prop::array::uniform10(0f64..)) {
+        let res = adds(array, array);
+        assert!(res.zip(array).into_iter().all(|(a, b)| a == 2. * b));
+    }
+
+    #[test]
+    fn test_sums(array in prop::array::uniform10(0f64..)) {
+        let res = sums(array);
+        assert_eq!(res, array.into_iter().sum());
+    }
+
+    #[test]
+    fn test_prods(array in prop::array::uniform10(0f64..)) {
+        let res = prods(array);
+        assert_eq!(res, array.into_iter().product());
+    }
 }
