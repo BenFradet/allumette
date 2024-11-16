2b69e8d7efbb7d84f54c602c0143d5affc403d21
benfradet <benjamin.fradet@gmail.com>
2024-11-16 21:55:21 +0100

    unary tests

--- src/ops/unary_ops.rs
+++ src/ops/unary_ops.rs
@@ -10,12 +10,19 @@ pub trait Unary {
 pub struct Log;
 impl Unary for Log {
     fn forward(&self, _ctx: &Context, a: f64) -> f64 {
-        a.ln()
+        if a <= 0. {
+            0.
+        } else {
+            a.ln()
+        }
     }
 
     fn backward(&self, ctx: &Context, d: f64) -> f64 {
         let vs = &ctx.saved_values;
-        let a = if vs.is_empty() { 0. } else { vs[0] };
+        let a = vs
+            .first()
+            .filter(|v| **v != 0.)
+            .unwrap_or(&1.);
         d / a
     }
 }
@@ -23,12 +30,19 @@ impl Unary for Log {
 pub struct Inv;
 impl Unary for Inv {
     fn forward(&self, _ctx: &Context, a: f64) -> f64 {
-        1. / a
+        if a == 0. {
+            0.
+        } else {
+            1. / a
+        }
     }
 
     fn backward(&self, ctx: &Context, d: f64) -> f64 {
         let vs = &ctx.saved_values;
-        let a = vs.first().unwrap_or(&0.);
+        let a = vs
+            .first()
+            .filter(|v| **v != 0.)
+            .unwrap_or(&1.);
         (-1. / (a.powf(2.))) * d
     }
 }
@@ -90,5 +104,95 @@ impl Unary for Exp {
         let vs = &ctx.saved_values;
         let a = vs.first().unwrap_or(&0.);
         a.exp() * d
+    }
+}
+
+use proptest::prelude::*;
+
+fn is_close(a: f64, b: f64) -> bool {
+    (a - b).abs() < 1e-4
+}
+
+proptest! {
+    #[test]
+    fn ln_tests(a in any::<f64>()) {
+        let log = Log {};
+        let ctx = Context::default();
+        let f = log.forward(&ctx, a);
+        if a <= 0. {
+            assert_eq!(0., f);
+        } else {
+            assert_eq!(a.ln(), f);
+        }
+        let back = log.backward(&ctx.push(a), a);
+        let exp = if a == 0. { 1. } else { a };
+        assert_eq!(a / exp, back);
+    }
+
+    #[test]
+    fn inv_tests(a in any::<f64>()) {
+        let inv = Inv {};
+        let ctx = Context::default();
+        let f = inv.forward(&ctx, a);
+        if a == 0. {
+            assert_eq!(0., f);
+        } else {
+            assert_eq!(1. / a, f);
+        }
+        let back = inv.backward(&ctx.push(a), a);
+        let exp = if a == 0. { 1. } else { a };
+        assert_eq!((-1. / (exp.powf(2.))) * a, back);
+    }
+
+    #[test]
+    fn neg_tests(a in any::<f64>()) {
+        let neg = Neg {};
+        let ctx = Context::default();
+        let f = neg.forward(&ctx, a);
+        assert_eq!(-a, f);
+        let back = neg.backward(&ctx, a);
+        assert_eq!(-a, back);
+    }
+
+    #[test]
+    fn sig_tests(a in any::<f64>()) {
+        let sig = Sig {};
+        let ctx = Context::default();
+        let f = sig.forward(&ctx, a);
+        if a >= 0. {
+            assert_eq!(1. / (1. + (-a).exp()), f);
+        } else {
+            assert_eq!(a.exp() / (1. + a.exp()), f);
+        }
+        let back = sig.backward(&ctx.push(a), a);
+        assert_eq!(f * (1. - f) * a, back);
+    }
+
+    #[test]
+    fn relu_tests(a in any::<f64>()) {
+        let relu = Relu {};
+        let ctx = Context::default();
+        let f = relu.forward(&ctx, a);
+        if a >= 0. {
+            assert_eq!(a, f);
+        } else {
+            assert_eq!(0., f);
+        }
+        let back = relu.backward(&ctx.push(a), a);
+        if a > 0. {
+            assert_eq!(a, back);
+        } else {
+            assert_eq!(0., back);
+        }
+    }
+
+    #[test]
+    fn exp_tests(a in any::<f64>()) {
+        let exp = Exp {};
+        let ctx = Context::default();
+        let f = exp.forward(&ctx, a);
+        assert_eq!(a.exp(), f);
+        let back = exp.backward(&ctx.push(a), a);
+        assert_eq!(a.exp() * a, back);
     }
 }
