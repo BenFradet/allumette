f8fd82d8eea2ca2f23c7141be395396ac414ee36
benfradet <benjamin.fradet@gmail.com>
2024-12-26 23:13:22 +0100

    shape and stride as part of tensor data

--- src/tensor/tensor.rs
+++ src/tensor/tensor.rs
@@ -3,6 +3,6 @@ use std::sync::Arc;
 use super::tensor_data::TensorData;
 
 #[derive(Clone)]
-pub struct Tensor {
-    data: Arc<TensorData>,
+pub struct Tensor<const N: usize> {
+    data: Arc<TensorData<N>>,
 }
--- src/tensor/tensor_data.rs
+++ src/tensor/tensor_data.rs
@@ -1,16 +1,21 @@
 // TODO: stride and shape part of tensor data?
 #[derive(Debug)]
-pub struct TensorData {
+pub struct TensorData<const N: usize> {
     data: Vec<f64>,
+    stride: Stride<N>,
+    shape: Shape<N>,
 }
 
-impl TensorData {
-    fn index_to_position<const N: usize>(idx: Index<N>, stride: Stride<N>) -> usize {
-        idx.data.iter().zip(stride.data.iter()).fold(0, |acc, (idx, stride)| acc + idx * stride)
+impl<const N: usize> TensorData<N> {
+    fn position(&self, idx: Index<N>) -> usize {
+        idx.data
+            .iter()
+            .zip(self.stride.data.iter())
+            .fold(0, |acc, (idx, stride)| acc + idx * stride)
     }
 
-    fn position_to_index<const N: usize>(pos: usize, shape: Shape<N>) -> Index<N> {
-        let stride = Self::stride_from_shape(shape);
+    fn index(&self, pos: usize) -> Index<N> {
+        let stride = Self::stride_from_shape(&self.shape);
         let mut res = [1; N];
         let mut mut_pos = pos;
         for i in 0..N {
@@ -23,10 +28,10 @@ impl TensorData {
     }
 
     // TODO: impl From
-    fn stride_from_shape<const N: usize>(shape: Shape<N>) -> Stride<N> {
+    fn stride_from_shape(shape: &Shape<N>) -> Stride<N> {
         let mut res = [1; N];
         let mut offset = 1;
-        for i in 0..N-1 {
+        for i in 0..N - 1 {
             let idx = N - i - 1;
             let s = shape.data[idx];
             res[i] = s * offset;
@@ -62,7 +67,7 @@ mod tests {
 
     #[test]
     fn stride_from_shape_test() -> () {
-        let res = TensorData::stride_from_shape(Shape { data: [5, 4] });
+        let res = TensorData::stride_from_shape(&Shape { data: [5, 4] });
         assert_eq!([4, 1], res.data);
     }
 }
