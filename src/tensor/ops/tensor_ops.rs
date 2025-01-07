b417e0fd918c6f048c222837e3a3e2ea0bf3df78
benfradet <benjamin.fradet@gmail.com>
2025-01-07 22:43:54 +0100

    unary ops

---
+++ src/tensor/ops/tensor_ops.rs
@@ -0,0 +1,3 @@
+// map
+// zip
+// reduce
--- src/tensor/ops/unary_ops.rs
+++ src/tensor/ops/unary_ops.rs
@@ -1,1 +1,10 @@
+pub trait UnaryOps {
+    type Placeholder;
 
+    fn exp(p: &Self::Placeholder) -> Self::Placeholder;
+    fn inv(p: &Self::Placeholder) -> Self::Placeholder;
+    fn log(p: &Self::Placeholder) -> Self::Placeholder;
+    fn neg(p: &Self::Placeholder) -> Self::Placeholder;
+    fn relu(p: &Self::Placeholder) -> Self::Placeholder;
+    fn sig(p: &Self::Placeholder) -> Self::Placeholder;
+}
