b77fba3bc1a65a19bd1cbaeb1c4657178822970e
benfradet <benjamin.fradet@gmail.com>
2025-01-08 22:14:21 +0100

    low level tensor ops

--- src/tensor/ops/mod.rs
+++ src/tensor/ops/mod.rs
@@ -1,2 +1,3 @@
 pub mod binary_ops;
+pub mod tensor_ops;
 pub mod unary_ops;
--- src/tensor/ops/tensor_ops.rs
+++ src/tensor/ops/tensor_ops.rs
@@ -1,3 +1,7 @@
-// map
-// zip
-// reduce
+pub trait TensorOps {
+    type Placeholder;
+
+    fn map(t: &Self::Placeholder, f: impl Fn(f64) -> f64) -> Self::Placeholder;
+    fn zip(t: &Self::Placeholder, f: impl Fn(f64, f64) -> f64) -> Self::Placeholder;
+    fn reduce(t: &Self::Placeholder, f: impl Fn(f64, f64) -> f64) -> Self::Placeholder;
+}
