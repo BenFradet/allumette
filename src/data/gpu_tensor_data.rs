edc45a8ed6a572e54a3003685e329924a5e0a63f
benfradet <benjamin.fradet@gmail.com>
2025-07-28 20:52:57 +0200

    start impl gpu tensor data

--- src/data/gpu_tensor_data.rs
+++ src/data/gpu_tensor_data.rs
@@ -1,10 +1,101 @@
 use std::sync::Arc;
 
-use crate::shaping::{shape::Shape, strides::Strides};
+use crate::{data::tensor_data::TensorData, shaping::{shape::Shape, strides::Strides}};
 
 #[derive(Clone, Debug)]
 pub struct GpuTensorData {
     buffer: Arc<wgpu::Buffer>,
     pub shape: Shape,
     pub strides: Strides,
+}
+
+impl TensorData for GpuTensorData {
+    fn shape(&self) -> &Shape {
+        self.shape
+    }
+
+    fn size(&self) -> usize {
+        self.shape.size
+    }
+
+    fn iter(&self) -> std::slice::Iter<'_, f64> {
+        todo!()
+    }
+
+    fn first(&self) -> Option<f64> {
+        todo!()
+    }
+
+    // TODO: factor out
+    fn is_contiguous(&self) -> bool {
+        if self.strides.is_empty() {
+            false
+        } else {
+            let mut last = self.strides[0];
+            for stride in self.strides.iter() {
+                if stride > last {
+                    return false;
+                }
+                last = stride;
+            }
+            true
+        }
+    }
+
+    fn reshape(&self, shape: Shape) -> Self {
+        let strides = (&shape).into();
+        Self {
+            buffer: Arc::clone(&self.buffer),
+            shape,
+            strides,
+        }
+    }
+
+    fn permute(&self, order: &Self) -> Option<Self>
+    where
+        Self: Sized {
+        todo!()
+    }
+
+    fn index(&self, idx: crate::shaping::idx::Idx) -> f64 {
+        todo!()
+    }
+
+    fn indices(&self) -> impl Iterator<Item = crate::shaping::idx::Idx> {
+        todo!()
+    }
+
+    fn ones(shape: Shape) -> Self {
+        todo!()
+    }
+
+    fn zeros(shape: Shape) -> Self {
+        todo!()
+    }
+
+    fn rand(shape: Shape) -> Self {
+        todo!()
+    }
+
+    fn epsilon(shape: Shape, idx: &crate::shaping::idx::Idx, eps: f64) -> Self {
+        todo!()
+    }
+
+    fn from(data: Vec<f64>, shape: Shape, strides: Strides) -> Self {
+        todo!()
+    }
+
+    fn scalar(s: f64) -> Self {
+        todo!()
+    }
+
+    fn vec(v: Vec<f64>) -> Self {
+        todo!()
+    }
+
+    fn matrix(m: Vec<Vec<f64>>) -> Option<Self>
+    where
+        Self: Sized {
+        todo!()
+    }
 }
