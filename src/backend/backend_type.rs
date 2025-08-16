4563791029da91f1b86520efb13639e7770ba3a1
benfradet <benjamin.fradet@gmail.com>
2025-08-16 22:08:13 +0200

    add gpu backend type

--- src/backend/backend_type.rs
+++ src/backend/backend_type.rs
@@ -4,8 +4,11 @@ pub trait TensorBackendType {}
 pub struct Seq;
 #[derive(Debug, Clone)]
 pub struct Par;
+#[derive(Debug, Clone)]
+pub struct Gpu;
 
 impl TensorBackendType for Seq {}
 impl TensorBackendType for Par {}
+impl TensorBackendType for Gpu {}
 
 pub trait BackendType = TensorBackendType + Clone + std::fmt::Debug;
