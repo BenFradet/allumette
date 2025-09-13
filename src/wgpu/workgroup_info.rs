dbd5f3db0da8789227bc8a9cc28e084eb9a1d6a7
benfradet <benjamin.fradet@gmail.com>
2025-09-13 20:05:56 +0200

    work group info from shape

--- src/wgpu/workgroup_info.rs
+++ src/wgpu/workgroup_info.rs
@@ -1,10 +1,56 @@
+use crate::shaping::shape::Shape;
+
 /// workgroup is a collection of `size` threads that execute together and share local memory
 /// each thread has a unique id, allowing it to process a specific portion of the data
 /// https://medium.com/@josh.sideris/mastering-thread-calculations-in-webgpu-workgroup-size-count-and-thread-identification-6b44a87a4764
 ///
 /// count: total number of workgroups
 /// size: number of threads per workgroup
+#[derive(Debug, Clone, Copy, PartialEq)]
 struct WorkgroupInfo {
     count: usize,
     size: usize,
+}
+
+const MAX_WORKGROUP_COUNT: usize = 65535;
+const MAX_WORKGROUP_SIZE: usize = 256;
+
+impl From<Shape> for WorkgroupInfo {
+    fn from(shape: Shape) -> Self {
+        let tensor_size = shape.size;
+
+        // TODO: None if tensor size > SIZE * COUNT
+        if tensor_size <= MAX_WORKGROUP_SIZE {
+            WorkgroupInfo {
+                count: 1,
+                size: tensor_size.next_power_of_two(),
+            }
+        } else {
+            let count = (tensor_size + MAX_WORKGROUP_SIZE - 1) / MAX_WORKGROUP_SIZE;
+            WorkgroupInfo {
+                count: count,
+                size: MAX_WORKGROUP_SIZE,
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn from_shape_test() {
+        assert_eq!(
+            WorkgroupInfo {
+                count: 2,
+                size: 256
+            },
+            Shape::new(vec![257]).into()
+        );
+        assert_eq!(
+            WorkgroupInfo { count: 1, size: 32 },
+            Shape::new(vec![14, 2]).into()
+        );
+    }
 }
