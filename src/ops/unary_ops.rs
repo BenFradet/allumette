085371cff6866740a0f5d9d94dc44c1cf41626a6
benfradet <benjamin.fradet@gmail.com>
2026-01-13 23:28:41 +0100

    associated type refactor wip, unary ops

--- src/ops/unary_ops.rs
+++ src/ops/unary_ops.rs
@@ -1,6 +1,6 @@
 use crate::{
     autodiff::context::Context,
-    backend::{backend::Backend, backend_type::BackendType},
+    backend::{backend::{Backend, TensorBackend}, backend_type::BackendType},
     data::tensor_data::TensorData,
     math::element::Element,
 };
@@ -8,14 +8,14 @@ use crate::{
 use super::unary::Unary;
 
 pub struct Neg;
-impl<E: Element, BT: BackendType, T: Backend<E, BT>> Unary<E, BT, T> for Neg {
-    fn forward(&self, a: &T) -> T {
+impl<'a, B: Backend> Unary<'a, B> for Neg {
+    fn forward(&self, a: &B::Storage<'a>) -> B::Storage<'a> {
         // TODO: make self.tag() work
-        a.map(|e| -e, <Neg as Unary<E, BT, T>>::tag(self))
+        a.map(|e| -e, <Neg as Unary<'a, B>>::tag(self))
     }
 
-    fn backward(&self, _ctx: &Context<T>, d: &T) -> T {
-        d.map(|e| -e, <Neg as Unary<E, BT, T>>::tag(self))
+    fn backward(&self, _ctx: &Context<B::Storage<'a>>, d: &B::Storage<'a>) -> B::Storage<'a> {
+        d.map(|e| -e, <Neg as Unary<'a, B>>::tag(self))
     }
 
     fn tag(&self) -> &'static str {
@@ -24,29 +24,29 @@ impl<E: Element, BT: BackendType, T: Backend<E, BT>> Unary<E, BT, T> for Neg {
 }
 
 pub struct Inv;
-impl<E: Element, BT: BackendType, T: Backend<E, BT>> Unary<E, BT, T> for Inv {
-    fn forward(&self, a: &T) -> T {
+impl<'a, B: Backend> Unary<'a, B> for Inv {
+    fn forward(&self, a: &B::Storage<'a>) -> B::Storage<'a> {
         a.map(
             |e| {
-                if e != E::zero() {
-                    E::one() / e
+                if e != B::Element::zero() {
+                    B::Element::one() / e
                 } else {
-                    E::zero()
+                    B::Element::zero()
                 }
             },
-            <Inv as Unary<E, BT, T>>::tag(self),
+            <Inv as Unary<'a, B>>::tag(self),
         )
     }
 
     // decomposed for gpu, deriv 1/x => -1/x^2
-    fn backward(&self, ctx: &Context<T>, d: &T) -> T {
+    fn backward(&self, ctx: &Context<B::Storage<'a>>, d: &B::Storage<'a>) -> B::Storage<'a> {
         let d_neg = d.map(|e| -e, "neg");
         ctx.fst
             .as_ref()
             .and_then(|a| a.zip(a, |e1, e2| e1 * e2, "mul"))
-            .map(|a2| self.forward(&a2))
+            .map(|a2| <Inv as Unary<'a, B>>::forward(self, &a2))
             .and_then(|a2_inv| d_neg.zip(&a2_inv, |e1, e2| e1 * e2, "mul"))
-            .unwrap_or(<T as TensorData<E>>::ones(d.shape().clone()))
+            .unwrap_or(<B::Storage<'a> as TensorData<B::Element>>::ones(d.shape().clone()))
     }
 
     fn tag(&self) -> &'static str {
@@ -55,25 +55,25 @@ impl<E: Element, BT: BackendType, T: Backend<E, BT>> Unary<E, BT, T> for Inv {
 }
 
 pub struct Ln;
-impl<E: Element, BT: BackendType, T: Backend<E, BT>> Unary<E, BT, T> for Ln {
-    fn forward(&self, a: &T) -> T {
+impl<'a, B: Backend> Unary<'a, B> for Ln {
+    fn forward(&self, a: &B::Storage<'a>) -> B::Storage<'a> {
         a.map(
-            |e| if e > E::zero() { e.ln() } else { E::zero() },
-            <Ln as Unary<E, BT, T>>::tag(self),
+            |e| if e > B::Element::zero() { e.ln() } else { B::Element::zero() },
+            <Ln as Unary<'a, B>>::tag(self),
         )
     }
 
-    fn backward(&self, ctx: &Context<T>, d: &T) -> T {
+    fn backward(&self, ctx: &Context<B::Storage<'a>>, d: &B::Storage<'a>) -> B::Storage<'a> {
         ctx.fst
             .as_ref()
             .and_then(|a| {
                 a.zip(
                     d,
-                    |e1, e2| if e1 == E::zero() { e2 } else { e2 / e1 },
+                    |e1, e2| if e1 == B::Element::zero() { e2 } else { e2 / e1 },
                     "ln_diff",
                 )
             })
-            .unwrap_or(<T as TensorData<E>>::ones(d.shape().clone()))
+            .unwrap_or(<B::Storage<'a> as TensorData<B::Element>>::ones(d.shape().clone()))
     }
 
     fn tag(&self) -> &'static str {
@@ -82,19 +82,19 @@ impl<E: Element, BT: BackendType, T: Backend<E, BT>> Unary<E, BT, T> for Ln {
 }
 
 pub struct Sig;
-impl<E: Element, BT: BackendType, T: Backend<E, BT>> Unary<E, BT, T> for Sig {
-    fn forward(&self, a: &T) -> T {
-        a.map(|e| e.sig(), <Sig as Unary<E, BT, T>>::tag(self))
+impl<'a, B: Backend> Unary<'a, B> for Sig {
+    fn forward(&self, a: &B::Storage<'a>) -> B::Storage<'a> {
+        a.map(|e| e.sig(), <Sig as Unary<'a, B>>::tag(self))
     }
 
     // sig'(x) = sig(x) * (1 - sig(x))
-    fn backward(&self, ctx: &Context<T>, d: &T) -> T {
+    fn backward(&self, ctx: &Context<B::Storage<'a>>, d: &B::Storage<'a>) -> B::Storage<'a> {
         ctx.fst
             .as_ref()
             .and_then(|t| {
-                let sig = self.forward(t);
+                let sig = <Sig as Unary<'a, B>>::forward(self, t);
                 let minus_sig = sig.map(|e| -e, "neg");
-                let one_minus_sig = <T as TensorData<E>>::from_scalar(E::one()).zip(
+                let one_minus_sig = <B::Storage<'a> as TensorData<B::Element>>::from_scalar(B::Element::one()).zip(
                     &minus_sig,
                     |e1, e2| e1 + e2,
                     "add",
@@ -102,7 +102,7 @@ impl<E: Element, BT: BackendType, T: Backend<E, BT>> Unary<E, BT, T> for Sig {
                 one_minus_sig.and_then(|oms| sig.zip(&oms, |e1, e2| e1 * e2, "mul"))
             })
             .and_then(|deriv| d.zip(&deriv, |e1, e2| e1 * e2, "mul"))
-            .unwrap_or(<T as TensorData<E>>::ones(d.shape().clone()))
+            .unwrap_or(<B::Storage<'a> as TensorData<B::Element>>::ones(d.shape().clone()))
     }
 
     fn tag(&self) -> &'static str {
@@ -111,16 +111,16 @@ impl<E: Element, BT: BackendType, T: Backend<E, BT>> Unary<E, BT, T> for Sig {
 }
 
 pub struct Relu;
-impl<E: Element, BT: BackendType, T: Backend<E, BT>> Unary<E, BT, T> for Relu {
-    fn forward(&self, a: &T) -> T {
-        a.map(|e| e.relu(), <Relu as Unary<E, BT, T>>::tag(self))
+impl<'a, B: Backend> Unary<'a, B> for Relu {
+    fn forward(&self, a: &B::Storage<'a>) -> B::Storage<'a> {
+        a.map(|e| e.relu(), <Relu as Unary<'a, B>>::tag(self))
     }
 
-    fn backward(&self, ctx: &Context<T>, d: &T) -> T {
+    fn backward(&self, ctx: &Context<B::Storage<'a>>, d: &B::Storage<'a>) -> B::Storage<'a> {
         ctx.fst
             .as_ref()
             .and_then(|a| a.zip(d, |e1, e2| e1.relu_diff(e2), "relu_diff"))
-            .unwrap_or(<T as TensorData<E>>::ones(d.shape().clone()))
+            .unwrap_or(<B::Storage<'a> as TensorData<B::Element>>::ones(d.shape().clone()))
     }
 
     fn tag(&self) -> &'static str {
@@ -129,17 +129,17 @@ impl<E: Element, BT: BackendType, T: Backend<E, BT>> Unary<E, BT, T> for Relu {
 }
 
 pub struct Exp;
-impl<E: Element, BT: BackendType, T: Backend<E, BT>> Unary<E, BT, T> for Exp {
-    fn forward(&self, a: &T) -> T {
-        a.map(|e| e.exp(), <Exp as Unary<E, BT, T>>::tag(self))
+impl<'a, B: Backend> Unary<'a, B> for Exp {
+    fn forward(&self, a: &B::Storage<'a>) -> B::Storage<'a> {
+        a.map(|e| e.exp(), <Exp as Unary<'a, B>>::tag(self))
     }
 
-    fn backward(&self, ctx: &Context<T>, d: &T) -> T {
+    fn backward(&self, ctx: &Context<B::Storage<'a>>, d: &B::Storage<'a>) -> B::Storage<'a> {
         ctx.fst
             .as_ref()
-            .map(|t| self.forward(t))
+            .map(|t| <Exp as Unary<'a, B>>::forward(self, t))
             .and_then(|exp| exp.zip(d, |e1, e2| e1 * e2, "mul"))
-            .unwrap_or(<T as TensorData<E>>::ones(d.shape().clone()))
+            .unwrap_or(<B::Storage<'a> as TensorData<B::Element>>::ones(d.shape().clone()))
     }
 
     fn tag(&self) -> &'static str {
@@ -149,17 +149,17 @@ impl<E: Element, BT: BackendType, T: Backend<E, BT>> Unary<E, BT, T> for Exp {
 
 // aka make contiguous
 pub struct Copy;
-impl<E: Element, BT: BackendType, T: Backend<E, BT>> Unary<E, BT, T> for Copy {
-    fn forward(&self, a: &T) -> T {
+impl<'a, B: Backend> Unary<'a, B> for Copy {
+    fn forward(&self, a: &B::Storage<'a>) -> B::Storage<'a> {
         a.map_broadcast(
-            &<T as TensorData<E>>::zeros(a.shape().clone()),
+            &<B::Storage<'a> as TensorData<B::Element>>::zeros(a.shape().clone()),
             |f| f,
-            <Copy as Unary<E, BT, T>>::tag(self),
+            <Copy as Unary<'a, B>>::tag(self),
         )
-        .unwrap_or(a.map(|f| f, <Copy as Unary<E, BT, T>>::tag(self)))
+        .unwrap_or(a.map(|f| f, <Copy as Unary<'a, B>>::tag(self)))
     }
 
-    fn backward(&self, _ctx: &Context<T>, d: &T) -> T {
+    fn backward(&self, _ctx: &Context<B::Storage<'a>>, d: &B::Storage<'a>) -> B::Storage<'a> {
         d.clone()
     }
 
