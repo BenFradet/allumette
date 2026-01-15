0ecc5e5044a7dd1c44126ca228d850ea5a48ef3f
benfradet <benjamin.fradet@gmail.com>
2026-01-15 21:31:41 +0100

    start implementing visual debugger

--- src/training/debugger.rs
+++ src/training/debugger.rs
@@ -1,5 +1,12 @@
 use std::time::Instant;
 
+use ratatui::style::{Color, Style, Stylize};
+use ratatui::symbols::Marker;
+use ratatui::text::Line;
+use ratatui::widgets::{Axis, Block, Chart, Dataset, GraphType};
+use ratatui::Frame;
+use ratatui::layout::{Constraint, Layout, Rect};
+
 use crate::backend::{backend::Backend, mode::Mode};
 use crate::shaping::shape::Shape;
 use crate::{math::element::Element, tensor::Tensor};
@@ -51,6 +58,54 @@ impl<'a, B: Backend> Debugger<'a, B> for TerseDebugger {
                 "iteration: {current}/{max}, elapsed time: {elapsed_time:?}, loss: {total_loss}, correct: {correct}"
             );
         }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct Point {
+    x: f64,
+    y: f64,
+    label: u8,
+    predicted: u8,
+}
+
+pub struct VizDebugger<'a> {
+    points: Vec<Point>,
+    loss: Vec<(usize, f64)>,
+    x_bounds: (f64, f64),
+    x_labels: Vec<&'a str>,
+    y_bounds: (f64, f64),
+    y_labels: Vec<&'a str>,
+    max_iterations: usize,
+    n: usize,
+}
+
+impl<'a> VizDebugger<'a> {
+    pub fn new(total_iterations: usize, n: usize) -> VizDebugger<'a> {
+        Self {
+            points: vec![],
+            loss: vec![],
+            x_bounds: (0., 0.),
+            x_labels: vec!["0.0"],
+            y_bounds: (0., 0.),
+            y_labels: vec!["0.0"],
+            max_iterations: total_iterations,
+            n,
+        }
+    }
+
+    pub fn draw(&self, frame: &mut Frame) {
+        let [scatter, line_chart] =
+            Layout::horizontal([Constraint::Fill(1); 2]).areas(frame.area());
+        self.render_scatter(frame, scatter);
+        self.render_line_chart(frame, line_chart);
+    }
+
+    fn render_scatter(&self, frame: &mut Frame, area: Rect) {
+    }
+
+    fn render_line_chart(&self, frame: &mut Frame, area: Rect) {
+
     }
 }
 
